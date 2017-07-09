4bba43122c2c1d24101c0316b7398859d1ea58de
Peter Stradinger <emlprime@gmail.com>
2017-07-09 13:48:47 -0700

    making the test file right.

--- test.py
+++ test.py
@@ -2,14 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from game import handle_event
 from sqlalchemy.orm import Session
+from fantasyx.models import Character
+from flask import Flask
+from flask_dotenv import DotEnv
 
 Base = declarative_base()
-engine=create_engine('postgresql://admin:admin@localhost:5432/fantasyx')
+engine=create_engine('postgresql://%(DB_USERNAME)s:%(DB_PASSWORD)s@%(DB_ENDPOINT)s:5432/fantasyx' % app.config)
+app = Flask(__name__)
+env = DotEnv()
+env.init_app(app, env_file='.env', verbose_mode=True)
 
 db_session = Session(bind=engine)
 
-msg = {'type': 'scores'}
-
-result = handle_event(msg['type'], msg, engine)
-
+result = db_session.query(Character).count()
 print result
