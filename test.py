1d0feb7fbcb9939fdb816b6db00a2b0e126388b3
Peter Stradinger <emlprime@gmail.com>
2017-07-09 13:51:23 -0700

    fixing test.py git push

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from game import handle_event
 from sqlalchemy.orm import Session
 from fantasyx.models import Character
 from flask import Flask
 from flask_dotenv import DotEnv
 
 Base = declarative_base()
-engine=create_engine('postgresql://%(DB_USERNAME)s:%(DB_PASSWORD)s@%(DB_ENDPOINT)s:5432/fantasyx' % app.config)
 app = Flask(__name__)
 env = DotEnv()
 env.init_app(app, env_file='.env', verbose_mode=True)
 
+engine=create_engine('postgresql://%(DB_USERNAME)s:%(DB_PASSWORD)s@%(DB_ENDPOINT)s:5432/fantasyx' % app.config)
 db_session = Session(bind=engine)
 
 result = db_session.query(Character).count()
